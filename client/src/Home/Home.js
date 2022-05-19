39ad7cadf6510ae3130bed0a8fabd631c487d502
Tarek <kheirtarek@hotmail.fr>
2022-05-19 19:27:21 +0200

    client update

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ShopList from '../Shops/ShopList';
 import './Home.css';
+import AppContext from '../App/AppContext';
+import smile from '../images/smile.png';
 
 const Home = () => {
+  const context = useContext(AppContext);
+  const { user: { username } } = context;
+
   return (
     <div className='home'>
-      <h1 id='home-shops'>Shops</h1>
+      <h1 className='home-shops'>
+        <span className='home-welcome '>Welcome</span>
+        <div className='home-username center'>
+          <span id='home-username'>{username}</span>
+        </div>
+        <img id='smile' src={smile} alt='smile' height={60} width={60} />
+      </h1>
       <ShopList />
     </div>
   );
